e28f221551ab3a7ea87a6ef35da4ff58773c9398
NashidForbes <NashidForbes@biohumans.com>
2021-05-08 23:41:34 -0400

    Updating Swagger Meta Data documetation UI. Added @Bean annotation for it to work.

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package guru.springfamework.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 @Configuration
 public class SwaggerConfig {
 
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
