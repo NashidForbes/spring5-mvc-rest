0895f195011dce833fd6d33db696af13304706fe
NashidForbes <NashidForbes@biohumans.com>
2021-05-08 23:31:03 -0400

    Updating Swagger Meta Data documetation UI.

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -3,9 +3,13 @@ package guru.springfamework.config;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -17,7 +21,22 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
 
+    }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Nashid Forbes", "https://www.biohumans.com/about/",
+                "nashidforbes@biohumans.com");
+        return new ApiInfo(
+                "Spring Framework 5 Course Swagger MetaData Example",
+                "Spring Framework 5: Beginner to Guru",
+                "1.0",
+                "Terms of Service: blah",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
     }
 }
